0476f9d713e49eb6f19cffa9f1670aa6f50aa047
chaitusvk <srikanta.chaitanya@gmail.com>
2020-12-26 22:35:23 +0530

    OCR working flask

---
+++ app.py
@@ -0,0 +1,125 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+
+
+#pytorch
+import torch
+from torch.autograd import Variable
+import utils
+import dataset
+from PIL import Image
+
+import models.crnn as crnn
+import params
+#import argparse
+
+
+
+
+
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+#MODEL_PATH = 'models/model_resnet.h5'
+model_path = 'netCRNN_40_1000.pth'
+
+# Load your trained model
+#model = load_model(MODEL_PATH)
+#model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+# net init
+nclass = len(params.alphabet) + 1
+model = crnn.CRNN(params.imgH, params.nc, nclass, params.nh)
+if torch.cuda.is_available():
+    model = model.cuda()
+
+# load model
+print('loading pretrained model from %s' % model_path)
+if params.multi_gpu:
+    model = torch.nn.DataParallel(model)
+model.load_state_dict(torch.load(model_path))
+
+converter = utils.strLabelConverter(params.alphabet)
+transformer = dataset.resizeNormalize((params.imgW, params.imgH))
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+
+    image = Image.open(img_path).convert('L')
+    image = transformer(image)
+    if torch.cuda.is_available():
+       image = image.cuda()
+    image = image.view(1, *image.size())
+    image = Variable(image)
+
+    model.eval()
+    preds = model(image)
+
+    _, preds = preds.max(2)
+    preds = preds.transpose(1, 0).contiguous().view(-1)
+
+    preds_size = Variable(torch.LongTensor([preds.size(0)]))
+    raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
+    sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
+    print('%-20s => %-20s' % (raw_pred, sim_pred))
+
+
+
+    return sim_pred
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = preds        #str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
